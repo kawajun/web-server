739fea7328615b196d5ae9af4714c081d8ac2b4e
kawajun <kawajun0719@icloud.com>
2022-09-28 08:41:36 +0900

    Heroku対応

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.post('/api/v1/quiz', function (req, res) {
     const answer = req.body.answer
     if(answer === "2" ){
-        res.send("正解");
+        res.redirect("/correct.html");
     } else {
-        res.send("不正解");
+        res.redirect("/wrong.html");
     }
     res.send(answer);
 });
@@ -26,6 +26,8 @@ app.get('/about', function (req, res) {
     res.send('aboutページ');
 });
   
-app.listen(3000, function(){
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, function(){
     console.log("I am running!");
 });
